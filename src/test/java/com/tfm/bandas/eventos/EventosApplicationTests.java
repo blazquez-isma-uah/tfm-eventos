2cfac7215e18c8cf48e9530098c238924b28b0f1
Ismael Blazquez Carabias <ismael.blazquez@edu.uah.es>
2025-09-09 20:25:38 +0200

    Creacion del proyecto

---
+++ src/test/java/com/tfm/bandas/eventos/EventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfm.bandas.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
